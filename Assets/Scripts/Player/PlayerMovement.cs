6736640ec1dd770db0eefd6c24b6d212a7ca6d5a
sarp <sarp@sarps-MacBook-Pro.local>
2023-12-15 18:43:52 +0300

    FaceTarget Method Added(Player face is rotate when enemy moves)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -44,6 +44,8 @@ public class PlayerMovement : MonoBehaviour
 
     public void faceTarget()
     {
-        
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x,0f,direction.z));
+        transform.rotation=Quaternion.Slerp(transform.rotation,lookRotation,Time.deltaTime * 5f);
     }
 }
